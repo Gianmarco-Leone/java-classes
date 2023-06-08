73775f38f8310fc9f231feeeaf7b6e7e37390655
Gianmarco-Leone <gianmarcoleone96@gmail.com>
2023-06-08 09:25:21 +0200

    end ex2 + add ex3

--- src/Exercise2.java
+++ src/Exercise2.java
@@ -1,2 +1,12 @@
+import org.w3c.dom.ls.LSOutput;
+
 public class Exercise2 {
+    public static void main(String[] args) {
+        // Crea array e inizializza con giorni della settimana
+        String[] daysOfWeek = {"Lunedi", "Martedi", "Mercoledi", "Giovedi", "Venerdi", "Sabato", "Domenica"};
+        // Stampa numero elementi array e stampa il 4 giorno della settimana
+        System.out.println(daysOfWeek.length);
+        System.out.println(daysOfWeek[3]);
+    }
+
 }
---
+++ src/Exercise3.java
@@ -0,0 +1,6 @@
+public class Exercise3 {
+    public static void main(String[] args) {
+        // Inizializza un array con numeri da 1 a 10
+        int[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    }
+}
